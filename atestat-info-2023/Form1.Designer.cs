4e1f20038d763b07105067ffbe8d181a024ba9c2
b0c-beep <bocmarioadrian98@gmail.com>
2023-04-15 15:06:40 +0300

    pus textboxuri si labeluri pentru vector

--- atestat-info-2023/Form1.Designer.cs
+++ atestat-info-2023/Form1.Designer.cs
@@ -31,6 +31,10 @@ namespace atestat_info_2023
         {
             this.label_titlu = new System.Windows.Forms.Label();
             this.button_alegeMetoda = new System.Windows.Forms.Button();
+            this.textbox_marimeVector = new System.Windows.Forms.TextBox();
+            this.textbox_valoriVector = new System.Windows.Forms.TextBox();
+            this.label_marimeVector = new System.Windows.Forms.Label();
+            this.label_valoriVector = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label_titlu
@@ -53,11 +57,48 @@ namespace atestat_info_2023
             this.button_alegeMetoda.UseVisualStyleBackColor = true;
             this.button_alegeMetoda.Click += new System.EventHandler(this.button_alegeMetoda_Click);
             // 
+            // textbox_marimeVector
+            // 
+            this.textbox_marimeVector.Location = new System.Drawing.Point(133, 227);
+            this.textbox_marimeVector.MaxLength = 2;
+            this.textbox_marimeVector.Name = "textbox_marimeVector";
+            this.textbox_marimeVector.Size = new System.Drawing.Size(57, 27);
+            this.textbox_marimeVector.TabIndex = 2;
+            // 
+            // textbox_valoriVector
+            // 
+            this.textbox_valoriVector.Location = new System.Drawing.Point(133, 329);
+            this.textbox_valoriVector.Name = "textbox_valoriVector";
+            this.textbox_valoriVector.Size = new System.Drawing.Size(431, 27);
+            this.textbox_valoriVector.TabIndex = 3;
+            // 
+            // label_marimeVector
+            // 
+            this.label_marimeVector.AutoSize = true;
+            this.label_marimeVector.Location = new System.Drawing.Point(643, 234);
+            this.label_marimeVector.Name = "label_marimeVector";
+            this.label_marimeVector.Size = new System.Drawing.Size(106, 20);
+            this.label_marimeVector.TabIndex = 4;
+            this.label_marimeVector.Text = "Marime Vector";
+            // 
+            // label_valoriVector
+            // 
+            this.label_valoriVector.AutoSize = true;
+            this.label_valoriVector.Location = new System.Drawing.Point(643, 329);
+            this.label_valoriVector.Name = "label_valoriVector";
+            this.label_valoriVector.Size = new System.Drawing.Size(93, 20);
+            this.label_valoriVector.TabIndex = 5;
+            this.label_valoriVector.Text = "Valori Vector";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(882, 553);
+            this.Controls.Add(this.label_valoriVector);
+            this.Controls.Add(this.label_marimeVector);
+            this.Controls.Add(this.textbox_valoriVector);
+            this.Controls.Add(this.textbox_marimeVector);
             this.Controls.Add(this.button_alegeMetoda);
             this.Controls.Add(this.label_titlu);
             this.Name = "Form1";
@@ -72,6 +113,10 @@ namespace atestat_info_2023
 
         private System.Windows.Forms.Label label_titlu;
         private System.Windows.Forms.Button button_alegeMetoda;
+        private System.Windows.Forms.TextBox textbox_marimeVector;
+        private System.Windows.Forms.TextBox textbox_valoriVector;
+        private System.Windows.Forms.Label label_marimeVector;
+        private System.Windows.Forms.Label label_valoriVector;
     }
 }
 
