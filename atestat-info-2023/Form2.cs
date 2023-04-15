d6bd80c84f5ac45f23256689154f08d4a9322f7b
b0c-beep <bocmarioadrian98@gmail.com>
2023-04-15 19:02:30 +0300

    setare butoane

--- atestat-info-2023/Form2.cs
+++ atestat-info-2023/Form2.cs
@@ -17,17 +17,23 @@ namespace atestat_info_2023
 
         private void button_selectionSort_Click(object sender, EventArgs e)
         {
-
+            SelectionSortInfo ssi = new SelectionSortInfo();
+            this.Close();
+            ssi.Show();
         }
 
         private void button_bubbleSort_Click(object sender, EventArgs e)
         {
-
+            BubbleSortInfo bsi = new BubbleSortInfo();
+            this.Close();
+            bsi.Show();
         }
 
         private void button_quickSort_Click(object sender, EventArgs e)
         {
-
+            QuickSortInfo qsi = new QuickSortInfo();
+            this.Close();
+            qsi.Show();
         }
     }
 }
