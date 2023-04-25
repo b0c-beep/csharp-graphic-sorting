8cdf9e6e49a21af6e53e1efc3207e052a83ad37d
b0c-beep <bocmarioadrian98@gmail.com>
2023-04-25 22:41:35 +0300

    somewhat works selection sort illustration

--- atestat-info-2023/SelectionSortIlustratie.cs
+++ atestat-info-2023/SelectionSortIlustratie.cs
@@ -10,35 +10,44 @@ namespace atestat_info_2023
 {
     public partial class SelectionSortIlustratie : Form
     {
+        PictureBox[] pictureBoxes = new PictureBox[Variables.n];
+        public int i = 0;
+        public int j = 0;
+
         public SelectionSortIlustratie()
         {
             InitializeComponent();
+        }
+
+        public void creeareVector()
+        {
+            for(int i = 0; i < Variables.n; i++)
+            {
+                pictureBoxes[i] = new PictureBox();
+                pictureBoxes[i].Name = "pbBox" + i;
+                pictureBoxes[i].Tag = "pbbox";
+                pictureBoxes[i].BackColor = Color.Blue;
+                Controls.Add(pictureBoxes[i]);
+                pictureBoxes[i].Visible = false;
+            }
         }
 
         public void afisareVector()
         {
             for(int i = 0; i < Variables.n; i++)
             {
-                PictureBox PBox1 = new PictureBox();
-
-                PBox1.Location = new Point(50 * i + 50, 50);
-                PBox1.Size = new System.Drawing.Size(50, Variables.v[i] * 50);
-                PBox1.BackColor = Color.Blue;
-                PBox1.Tag = "patrat";
-                Controls.Add(PBox1);
+          
+                pictureBoxes[i].Location = new Point(50 * i + 50, 50);
+                pictureBoxes[i].Size = new System.Drawing.Size(50, Variables.v[i] * 50);
+                pictureBoxes[i].Visible = true;
+                
             }
         }
         public void stergereVector()
         {
             for (int i = 0; i < Variables.n; i++)
             {
-                PictureBox PBox1 = new PictureBox();
-
-                PBox1.Location = new Point(50 * i + 50, 50);
-                PBox1.Size = new System.Drawing.Size(50, Variables.v[i] * 50);
-                PBox1.BackColor = Color.Transparent;
-
-                Controls.Add(PBox1);
+                pictureBoxes[i].Visible = false;
             }
         }
 
@@ -47,26 +56,56 @@ namespace atestat_info_2023
             timerSelectionSort.Enabled = true;
             timerSelectionSort.Interval = 1000;
 
+            i = 0;
+            j = i + 1;
+
             this.Size = new Size(50 * (Variables.n + 2) + 20, 50 * (Variables.maxi + 3));
+            creeareVector();
             afisareVector();
-            stergereVector();
         }
 
-        private void timerSelectionSort_Tick(object sender, EventArgs e)
+
+        
+        public void sortareVector()
         {
-            /*
-            for (int i = 0; i < Variables.n - 1; i++) { 
-                for (int j = i + 1; j < Variables.n; j++) { 
-                    if (Variables.v[i] > Variables.v[j]) {
+            
+
+                    if (Variables.v[i] > Variables.v[j])
+                    {
+                        
                         int aux = Variables.v[i];
                         Variables.v[i] = Variables.v[j];
                         Variables.v[j] = aux;
                     }
-                    afisareVector();
-                    stergereVector();
+
+          
+        }
+
+
+        private void timerSelectionSort_Tick(object sender, EventArgs e)
+        {
+
+            sortareVector();
+            afisareVector();
+            //stergereVector();
+           
+            if(i < Variables.n - 1)
+            {
+                if (j < Variables.n)
+                {
+                    j++;
+                }
+                else
+                {
+                    i++;
+                    j = i + 1;
                 }
             }
-            */
+            else
+            {
+                timerSelectionSort.Enabled = false;
+            }
+            
         }
     }
 }
