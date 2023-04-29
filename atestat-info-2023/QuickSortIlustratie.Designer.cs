16cc5a8a566fe7d991fd6e4a8f6a8b16f8936213
b0c-beep <bocmarioadrian98@gmail.com>
2023-04-29 16:39:03 +0300

    setup quicksort

---
+++ atestat-info-2023/QuickSortIlustratie.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace atestat_info_2023
+{
+    partial class QuickSortIlustratie
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelInterschimbari = new System.Windows.Forms.Label();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonMeniu = new System.Windows.Forms.Button();
+            this.timerQuickSort = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // labelInterschimbari
+            // 
+            this.labelInterschimbari.AutoSize = true;
+            this.labelInterschimbari.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelInterschimbari.Location = new System.Drawing.Point(267, 12);
+            this.labelInterschimbari.Name = "labelInterschimbari";
+            this.labelInterschimbari.Size = new System.Drawing.Size(99, 13);
+            this.labelInterschimbari.TabIndex = 5;
+            this.labelInterschimbari.Text = "Interschimbari: ";
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(139, 12);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.TabIndex = 4;
+            this.buttonExit.Text = "Iesire";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // buttonMeniu
+            // 
+            this.buttonMeniu.Location = new System.Drawing.Point(23, 12);
+            this.buttonMeniu.Name = "buttonMeniu";
+            this.buttonMeniu.Size = new System.Drawing.Size(110, 23);
+            this.buttonMeniu.TabIndex = 3;
+            this.buttonMeniu.Text = "Inapoi la meniu";
+            this.buttonMeniu.UseVisualStyleBackColor = true;
+            this.buttonMeniu.Click += new System.EventHandler(this.buttonMeniu_Click);
+            // 
+            // timerQuickSort
+            // 
+            this.timerQuickSort.Tick += new System.EventHandler(this.timerQuickSort_Tick);
+            // 
+            // QuickSortIlustratie
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelInterschimbari);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonMeniu);
+            this.Name = "QuickSortIlustratie";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Atestat";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.QuickSortIlustratie_FormClosing);
+            this.Load += new System.EventHandler(this.QuickSortIlustratie_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelInterschimbari;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonMeniu;
+        private System.Windows.Forms.Timer timerQuickSort;
+    }
+}
---
+++ atestat-info-2023/QuickSortIlustratie.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace atestat_info_2023
+{
+    public partial class QuickSortIlustratie : Form
+    {
+        PictureBox[] pictureBoxes = new PictureBox[100];
+        public int i = 0;
+        public int j = 0;
+        public int last_i = 0, last_j = 0;
+        public Point intermPoint;
+        public Size intermSize;
+        public int interschimbari = 0;
+
+        public QuickSortIlustratie()
+        {
+            InitializeComponent();
+        }
+
+        public void creeareVector()
+        {
+            int i;
+            for (i = 0; i < Variables.n; i++)
+            {
+                pictureBoxes[i] = new PictureBox();
+                pictureBoxes[i].Name = "pbBox" + i;
+                pictureBoxes[i].Tag = "pbbox";
+                pictureBoxes[i].BackColor = Color.Blue;
+                Controls.Add(pictureBoxes[i]);
+                pictureBoxes[i].Visible = false;
+            }
+
+        }
+
+        public void afisareVector()
+        {
+
+            for (int c = 0; c < Variables.n; c++)
+            {
+
+                pictureBoxes[c].Location = new Point(50 * c + 50, 50);
+                pictureBoxes[c].Size = new System.Drawing.Size(50, Variables.v[c] * 50);
+                pictureBoxes[c].Visible = true;
+
+            }
+        }
+        public void stergereVector()
+        {
+            for (int i = 0; i < Variables.n; i++)
+            {
+                pictureBoxes[i].Visible = false;
+            }
+        }
+
+        public void sortareVector()
+        {
+            if (Variables.v[j] > Variables.v[j + 1])
+            {
+                interschimbari++;
+                labelInterschimbari.Text = "Interschimbari: " + interschimbari;
+
+                intermPoint = pictureBoxes[j].Location;
+                intermSize = pictureBoxes[j].Size;
+
+                pictureBoxes[j].Location = pictureBoxes[j + 1].Location;
+                pictureBoxes[j].Size = pictureBoxes[j + 1].Size;
+
+                pictureBoxes[j + 1].Location = intermPoint;
+                pictureBoxes[j + 1].Size = intermSize;
+
+                int aux = Variables.v[j];
+                Variables.v[j] = Variables.v[j + 1];
+                Variables.v[j + 1] = aux;
+                //sortat = false;
+            }
+        }
+
+        private void QuickSortIlustratie_Load(object sender, EventArgs e)
+        {
+            interschimbari = 0;
+
+            buttonExit.Visible = true;
+            buttonExit.Enabled = true;
+
+
+            buttonMeniu.Visible = true;
+            buttonMeniu.Visible = true;
+
+
+            timerQuickSort.Enabled = true;
+            timerQuickSort.Interval = Variables.interval;
+
+            i = 0;
+            j = 0;
+
+            this.Size = new Size(50 * (Variables.n + 2) + 20, 50 * (Variables.maxi + 3));
+
+            buttonMeniu.Top = 25;
+            buttonMeniu.Left = 15;
+
+            buttonExit.Top = 25;
+            buttonExit.Left = 25 + buttonMeniu.Width;
+
+            labelInterschimbari.Text = "Interschimbari: 0";
+            labelInterschimbari.Top = 5;
+            labelInterschimbari.Left = 15;
+
+            creeareVector();
+            afisareVector();
+        }
+
+        private void QuickSortIlustratie_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void buttonMeniu_Click(object sender, EventArgs e)
+        {
+            Form1 f1 = new Form1();
+            f1.Show();
+            this.Dispose();
+        }
+
+        private void timerQuickSort_Tick(object sender, EventArgs e)
+        {
+            sortareVector();
+            afisareVector();
+
+            if (i < Variables.n - 1)
+            {
+                if (j < Variables.n - i - 1)
+                {
+                    j++;
+                    if (j == Variables.n - i - 1)
+                        j = 0;
+                }
+                else
+                {
+                    i++;
+
+                    j = 0;
+                }
+            }
+            else
+            {
+
+                timerQuickSort.Enabled = false;
+
+            }
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
